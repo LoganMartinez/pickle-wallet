6dd6be1edcdc95ea2014cc923e4410b74468e6c9
LoganMartinez <lmartin9@trinity.edu>
2024-05-09 09:35:01 -0500

    Initial Commit

---
+++ PickleWallet/PickleWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickleWalletApp.swift
+//  PickleWallet
+//
+//  Created by Logan on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct PickleWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
